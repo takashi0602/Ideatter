5533391b695778fb63fb207ba5972d71c05ed36b
takashi <takashioicsn@gmail.com>
2018-09-22 13:41:10 +0900

    コントローラー モデル 作成

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,31 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// 投稿
+Route::get('/tweet', function () {
+  return view('tweet');
+});
+
+// タイムライン
+Route::get('/timeline', function () {
+  return view('timeline');
+});
+
+// ユーザー
+Route::get('/user', function () {
+  return view('user');
+});
+
+// 検索
+Route::get('/search', function () {
+  return view('search');
+});
+
+// ヘッダー
+Route::get('/header', function () {
+  return view('header');
+});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
