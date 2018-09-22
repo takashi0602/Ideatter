a42f4f2153198ee4d612c0959a03544c73134856
a-touda <b8241@oic.jp>
2018-09-22 17:32:00 +0900

    検索部の実装（見掛け倒し）

---
+++ resources/views/search.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+ <head>
+ <!-- Styles -->
+ <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+ </head>
+ <body>
+ <input class="kensaku" type="search" name="search" placeholder="タグ検索">
+ <input class="botan" type="submit" name="search" value="検索">
+ <br>
+ <br>
+ <p>＃C言語</p>
+ <p>#Python</p>
+ <p>#短期間</p>
+ </body>
+
+ <style>
+ .kensaku{
+  border-radius: 10px;
+ }
+ .botan{
+  border-radius: 10px;
+ }
+ </style>
