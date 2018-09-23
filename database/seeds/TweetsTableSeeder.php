3cda1201e934e926798674c0cadc556e89d4f168
takashi <takashioicsn@gmail.com>
2018-09-23 09:44:06 +0900

    create seeder

--- database/seeds/TweetsTableSeeder.php
+++ database/seeds/TweetsTableSeeder.php
@@ -15,18 +15,22 @@ class TweetsTableSeeder extends Seeder
         Tweet::truncate();
 
         $user_id = [
-          '1', '2', '3'
+          '1', '5', '2', '3', '4'
         ];
 
         $title = [
-          'hoge', 'piyo', 'pico'
+          'C言語で〇〇', 'Unityでゲーム開発', '勉強効率をあげる', '画期的な大発明', 'チーム開発'
         ];
 
         $description = [
-          'hogehogehogehoge', 'piyopiyopiyopiyo', 'picopicopicopico'
+          'C言語で〇〇しよう。',
+          'Unityでゲーム開発するときに知っておくと便利な...',
+          '勉強効率をあげるには短時間で...',
+          '画期的な大発明をしました。',
+          'チーム開発で役立つアイデアを...'
         ];
 
-        for($i = 0; $i < 3; $i++) {
+        for($i = 0; $i < 5; $i++) {
           DB::table('tweets')->insert([
             'user_id' => $user_id[$i],
             'title' => $title[$i],
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -15,18 +15,18 @@ class UsersTableSeeder extends Seeder
         User::truncate();
 
         $name = [
-          'サル', 'ゴリラ', 'チンパンジー'
+          '山田太郎', '田中花子', '鈴木一郎', '香川真司', '元木大介'
         ];
 
         $email = [
-          'monkey@monkey.com', 'gorilla@gorilla.com', 'chimpanzee@chimpanzee.com'
+          'idea1-tter@idea.com', 'idea2-tter@idea.com', 'idea3-tter@idea.com', 'idea4-tter@idea.com', 'idea5-tter@idea.com'
         ];
 
         $password = [
-          'passWord1', 'passWord2', 'passWord3'
+          'passWord1', 'passWord2', 'passWord3', 'passWord4', 'passWord5'
         ];
 
-        for($i = 0; $i < 3; $i++) {
+        for($i = 0; $i < 5; $i++) {
           DB::table('users')->insert([
             'name' => $name[$i],
             'email' => $email[$i],
